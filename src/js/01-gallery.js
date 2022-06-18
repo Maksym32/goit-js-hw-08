a55bb7e6fbe0004b2943e11e68df1fc3f30d8e40
Maksym32 <bednogo1@gmail.com>
2022-06-18 20:35:17 +0300

    hw 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const image = createGalleryItem(galleryItems);
+gallery.addEventListener('click', onImageClick);
+gallery.insertAdjacentHTML('afterbegin', image);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) =>
+    `<a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`).join("");
+
+};
+
+function onImageClick(event) {
+  
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return
+  }
+  const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt',
+  captionDelay: 250
+});
+};
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,77 @@
+import throttle from "lodash.throttle";
 
+// const STORAGE_KEY = 'feedback-form-state';
+// const formData = {}
+
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   textarea: document.querySelector('.feedback-form textarea')
+// };
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+// // refs.form.addEventListener('input', (e) => {
+  
+// //   formData[e.target.name] = e.target.value
+// //   console.log(formData)
+// // })
+
+// populateTextarea()
+
+// function onFormSubmit(evt) { 
+//   evt.preventDefault();
+
+//   evt.currentTarget.reset()
+//   localStorage.removeItem(STORAGE_KEY)
+// };
+
+
+
+// function onTextareaInput(evt) {
+//   const message = evt.target.value;
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// };
+
+
+
+// function populateTextarea() {
+//   const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+//   if (savedMessage) {
+//     refs.textarea.value = savedMessage;
+//   }
+// }
+
+
+
+
+const form = document.querySelector('.feedback-form');
+const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+
+populateForm();
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.target.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log(formData);
+  
+}
+
+function populateForm() {
+  const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (savedMessage) {
+    form.elements.email.value = savedMessage.email;
+    form.elements.message.value = savedMessage.message;
+  }
+}
