52746c8f29116139f02f1f6a93310bc744b164aa
Maksym32 <bednogo1@gmail.com>
2022-06-18 20:52:56 +0300

    hw8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,49 +1,5 @@
 import throttle from "lodash.throttle";
 
-// const STORAGE_KEY = 'feedback-form-state';
-// const formData = {}
-
-// const refs = {
-//   form: document.querySelector('.feedback-form'),
-//   textarea: document.querySelector('.feedback-form textarea')
-// };
-// refs.form.addEventListener('submit', onFormSubmit);
-// refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-
-// // refs.form.addEventListener('input', (e) => {
-  
-// //   formData[e.target.name] = e.target.value
-// //   console.log(formData)
-// // })
-
-// populateTextarea()
-
-// function onFormSubmit(evt) { 
-//   evt.preventDefault();
-
-//   evt.currentTarget.reset()
-//   localStorage.removeItem(STORAGE_KEY)
-// };
-
-
-
-// function onTextareaInput(evt) {
-//   const message = evt.target.value;
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// };
-
-
-
-// function populateTextarea() {
-//   const savedMessage = localStorage.getItem(STORAGE_KEY);
-
-//   if (savedMessage) {
-//     refs.textarea.value = savedMessage;
-//   }
-// }
-
-
-
 
 const form = document.querySelector('.feedback-form');
 const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
