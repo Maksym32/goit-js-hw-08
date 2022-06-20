30be7e2b386e6a26a7431009f5709f1f3af6f8c0
Maksym32 <bednogo1@gmail.com>
2022-06-20 16:07:50 +0300

    hw8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,6 +8,10 @@ player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({seconds}) {
   localStorage.setItem('videoplayer-current-time', seconds)
-}
+};
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+const persistedTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+if (persistedTime) {
+  player.setCurrentTime(persistedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,7 +4,7 @@ import throttle from "lodash.throttle";
 const form = document.querySelector('.feedback-form');
 const formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
 
-populateForm();
+populateForm()
 
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
@@ -15,19 +15,27 @@ function onFormInput(e) {
   formData[e.target.name] = e.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
+
 function onFormSubmit(e) {
   e.preventDefault();
+
+  if (form.elements.email.value === '' || form.elements.message.value === '') {
+    return alert('you must fill in all fields')
+  }
+  
+  console.log(formData);
   e.target.reset();
   localStorage.removeItem('feedback-form-state');
-  console.log(formData);
-  
-}
+
+  formData.email = '';
+  formData.message = '';
+};
 
 function populateForm() {
   const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
 
   if (savedMessage) {
-    form.elements.email.value = savedMessage.email;
-    form.elements.message.value = savedMessage.message;
+    form.elements.email.value = savedMessage.email || '';
+    form.elements.message.value = savedMessage.message || '';
   }
 }
